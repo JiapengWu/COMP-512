8bafdfc37dc9909f116e0b5c03ecf0e3c8bcf334
JiapengWu <jiapeng.wu@mail.mcgill.ca>
2018-09-30 17:49:42 -0400

    WIP

--- run_client.sh
+++ run_client.sh
@@ -1,4 +1,3 @@
 ##usage: ./run_client.sh [<middleware_hostname>] [port]
 
-cd ~/COMP-512/target
-java -cp COMP-512-Project1-0.0.1-SNAPSHOT-jar-with-dependencies.jar  main.java.Client.Client.TCPClient $1 $2
+java -cp  ${JARPATH} main.java.Client.Client.TCPClient $1 $2
