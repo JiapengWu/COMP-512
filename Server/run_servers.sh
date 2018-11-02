8c245ae97fed6f11e69c194954220c4b3f6fbe53
JiapengWu <jiapeng.wu@mail.mcgill.ca>
2018-11-02 14:22:00 -0400

    WIP

--- Server/run_servers.sh
+++ Server/run_servers.sh
@@ -1,6 +1,6 @@
 #!/bin/bash 
 
-MACHINES=(cs-2 cs-3 cs-4 cs-11)
+if [[ `pwd` == *"jwu558"* ]]; then MACHINES=(cs-5 cs-6 cs-7 cs-12); else MACHINES=(cs-2 cs-3 cs-4 cs-11) ; fi
 
 tmux new-session \; \
 	split-window -h \; \
